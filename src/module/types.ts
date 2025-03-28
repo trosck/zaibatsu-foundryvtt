a98c0937f576b443c5d80ad78bc6722d95cbdcb5
Sergei <kort.dll@gmail.com>
2025-03-28 14:20:08 +0300

    add EquippedStateEnum

--- src/module/types.ts
+++ src/module/types.ts
@@ -11,8 +11,18 @@ export const CharacteristicEnum = dictFromArray<Characteristic>(
   ZAIBATSU.CHARACTERISTICS,
 );
 
-type RetrogenicAdaptation = keyof typeof ZAIBATSU.RETROGENICS;
+export type RetrogenicAdaptation = keyof typeof ZAIBATSU.RETROGENICS;
 
 export const RetrogenicAdaptationEnum = dictFromArray<RetrogenicAdaptation>(
   Object.keys(ZAIBATSU.RETROGENICS),
+);
+
+export const EquippedStateEnum = dictFromArray<
+  (typeof ZAIBATSU.EQUIPPED_STATES)[number]
+>(ZAIBATSU.EQUIPPED_STATES);
+
+export type Weapon = keyof typeof ZAIBATSU.WEAPON_RANGES;
+
+export const WeaponEnum = dictFromArray<Weapon>(
+  Object.keys(ZAIBATSU.WEAPON_RANGES),
 );
