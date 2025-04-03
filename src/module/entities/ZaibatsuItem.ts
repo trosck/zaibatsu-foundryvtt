b59b0aa5ec4e7990f8e986c2f2c306d847d3a433
Sergei <kort.dll@gmail.com>
2025-04-03 11:29:38 +0300

    refactor item document class, add armor type

--- src/module/entities/ZaibatsuItem.ts
+++ src/module/entities/ZaibatsuItem.ts
@@ -1,8 +1,24 @@
 // @ts-nocheck
 
+import { ItemTypeEnum } from "../types";
 import { assetsPath } from "../utils/path";
 
 export class ZaibatsuItem extends Item {
+  constructor(item, options) {
+    super(item, options);
+
+    if (this.constructor === ZaibatsuItem) {
+      switch (item.type) {
+        case ItemTypeEnum.weapon:
+          return new ZaibatsuWeaponItem(item, options);
+        case ItemTypeEnum.armor:
+          return new ZaibatsuArmorItem(item, options);
+      }
+    }
+  }
+}
+
+class ZaibatsuWeaponItem extends ZaibatsuItem {
   protected async _preCreate(
     data: object,
     options: object,
@@ -10,10 +26,34 @@ export class ZaibatsuItem extends Item {
   ): Promise<boolean | void> {
     const result: boolean = await super._preCreate(data, options, userId);
 
-    await this.updateSource({
-      img: assetsPath("images", "weapons", `${this.system.weaponType}.png`),
-    });
+    if (isDefaultImage(this.img)) {
+      await this.updateSource({
+        img: assetsPath("images", "weapons", `${this.system.weaponType}.png`),
+      });
+    }
 
     return result;
   }
+}
+
+class ZaibatsuArmorItem extends ZaibatsuItem {
+  protected async _preCreate(
+    data: object,
+    options: object,
+    userId: User,
+  ): Promise<boolean | void> {
+    const result: boolean = await super._preCreate(data, options, userId);
+
+    if (isDefaultImage(this.img)) {
+      await this.updateSource({
+        img: assetsPath("images", "armor.png"),
+      });
+    }
+
+    return result;
+  }
+}
+
+function isDefaultImage(img: string) {
+  return img === foundry.documents.BaseItem.DEFAULT_ICON;
 }
